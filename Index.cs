8b3deb86359e6530012df35b2e7fa6fec8a9f282
Larry Knibb <larry@knibb.co.uk>
2012-10-23 21:47:15 +0800

    fixed culture bug causing BinarySearch on Hanzi index to fail

--- Index.cs
+++ Index.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.IO.IsolatedStorage;
 using System.Text;
+using System.Threading;
 
 namespace CC_CEDICT.WindowsPhone
 {
@@ -38,25 +40,48 @@ namespace CC_CEDICT.WindowsPhone
             }
         }
 
+        static CultureInfo en_US = new CultureInfo("en-US");
         int BinarySearch(string key)
         {
-            int min = 0, pos, max = this.Count - 1;
-            while (min <= max)
+            CultureInfo old = null;
+            if (Thread.CurrentThread.CurrentCulture.Name != en_US.Name) // Hanzi might not sort properly
             {
-                pos = (min + max) / 2;
-                switch (key.CompareTo(this[pos].Key))
+                old = Thread.CurrentThread.CurrentCulture;
+                Thread.CurrentThread.CurrentCulture = en_US;
+                Debug.WriteLine("Culture: {0} changed to {1}", old.Name, en_US.Name);
+            }
+            try
+            {
+                int min = 0, pos, max = this.Count - 1;
+                while (min <= max)
                 {
-                    case -1:
-                        max = pos - 1;
-                        break;
-                    case 0:
-                        return pos;
-                    case 1:
-                        min = pos + 1;
-                        break;
+                    pos = (min + max) / 2;
+                    switch (key.CompareTo(this[pos].Key))
+                    {
+                        case -1:
+                            max = pos - 1;
+                            Debug.WriteLine("BinarySearch: compare {0} to {1} (down)", key, this[pos].Key);
+                            break;
+                        case 0:
+                            Debug.WriteLine("BinarySearch: compare {0} to {1} (MATCH)", key, this[pos].Key);
+                            return pos;
+                        case 1:
+                            min = pos + 1;
+                            Debug.WriteLine("BinarySearch: compare {0} to {1} (up)", key, this[pos].Key);
+                            break;
+                    }
+                }
+                Debug.WriteLine("BinarySearch: not found.");
+                return -1;
+            }
+            finally // restore CultureInfo if changed earlier
+            {
+                if (old != null)
+                {
+                    Thread.CurrentThread.CurrentCulture = old;
+                    Debug.WriteLine("Culture: reset to {0}", old.Name);
                 }
             }
-            return -1;
         }
     }
 }
